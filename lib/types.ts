de04fda3c8dae31c6929607558104121637fbe98
fabriborgiXcode <pinocolbotto@gmail.com>
2026-08-18 13:09:42 +0200

    feat: purchase_item, generica e riusabile da F4

--- lib/types.ts
+++ lib/types.ts
@@ -344,6 +344,13 @@ export type Database = {
         Args: { p_id: string; p_reader: Database["public"]["Enums"]["person"] }
         Returns: undefined
       }
+      purchase_item: {
+        Args: {
+          p_actor: Database["public"]["Enums"]["person"]
+          p_item_key: string
+        }
+        Returns: undefined
+      }
       skip_question: {
         Args: {
           p_person: Database["public"]["Enums"]["person"]
